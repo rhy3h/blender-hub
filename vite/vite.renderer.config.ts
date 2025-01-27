8d5d88f2b64b4a8a4247a7cc5e11961caed63549
rhy3h <rhy3htw@gmail.com>
2025-01-27 23:22:31 +0800

    feat: Add vue as front end

--- vite/vite.renderer.config.ts
+++ vite/vite.renderer.config.ts
@@ -1,4 +1,29 @@
-import { defineConfig } from 'vite';
+import type { ConfigEnv, UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import { pluginExposeRenderer } from './vite.base.config'
+
+import path from 'path'
+
+import vue from '@vitejs/plugin-vue'
+import alias from '@rollup/plugin-alias'
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<'renderer'>
+  const { root, mode, forgeConfigSelf } = forgeEnv
+  const name = forgeConfigSelf.name ?? ''
+
+  return {
+    root,
+    mode,
+    base: './',
+    build: {
+      outDir: `.vite/renderer/${name}`
+    },
+    plugins: [pluginExposeRenderer(name), vue(), alias()],
+    resolve: {
+      preserveSymlinks: true
+    },
+    clearScreen: false
+  } as UserConfig
+})
