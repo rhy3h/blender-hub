2a882fe86f4951c46d8684192d5d6cd19bd41657
rhy3h <rhy3htw@gmail.com>
2025-05-06 10:48:36 +0800

    refactor: move webpack config files to dedicated folder and update imports

--- forge.config.ts
+++ forge.config.ts
@@ -8,8 +8,8 @@ import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { mainConfig } from './webpack/webpack.main.config';
+import { rendererConfig } from './webpack/webpack.renderer.config';
 
 const config: ForgeConfig = {
   packagerConfig: {
--- webpack/webpack.renderer.config.ts
+++ webpack/webpack.renderer.config.ts
@@ -1,6 +1,6 @@
 import type { Configuration } from 'webpack';
 
-import { rules } from './webpack.rules';
+import { rules } from '../webpack/webpack.rules';
 import { plugins } from './webpack.plugins';
 
 rules.push({
