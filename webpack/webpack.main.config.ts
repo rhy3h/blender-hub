482a60cc89a688100b285acd975b902541a0d2e4
rhy3h <rhy3htw@gmail.com>
2025-05-06 11:08:33 +0800

    refactor: simplify webpack config imports

--- webpack/webpack.main.config.ts
+++ webpack/webpack.main.config.ts
@@ -1,7 +1,7 @@
 import type { Configuration } from 'webpack';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
-import { rules } from '../webpack/webpack.rules';
+import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
 export const mainConfig: Configuration = {
