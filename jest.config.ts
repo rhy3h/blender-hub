f1fc05c8cbca81338eab8db96895103bf8a62276
rhy3h <rhy3htw@gmail.com>
2025-05-13 17:55:08 +0800

    chore: sync tsconfig paths with jest using pathsToModuleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,12 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
 export default {
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths || {},
+    { prefix: '<rootDir>/' },
+  ),
   transform: {
     '^.+\.tsx?$': ['ts-jest', {}],
   },
