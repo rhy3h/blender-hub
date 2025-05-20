27ac34ab5ef9a18f178c5da11ca7ac9bb8626c8d
rhy3h <rhy3htw@gmail.com>
2025-05-20 13:40:03 +0800

    feat: blender table remove filter

--- src/frontend/components/blender-table/page.tsx
+++ src/frontend/components/blender-table/page.tsx
@@ -4,45 +4,6 @@ import { columns } from '@/frontend/components/blender-table/columns';
 
 import { DataTable } from '@/frontend/components/ui/data-table/data-table';
 
-const oses = [
-  {
-    value: 'Windows',
-    label: 'Windows',
-  },
-  {
-    value: 'macOS',
-    label: 'macOS',
-  },
-  {
-    value: 'Linux',
-    label: 'Linux',
-  },
-];
-
-const arches = [
-  {
-    value: 'x64',
-    label: 'x64',
-  },
-  {
-    value: 'arm',
-    label: 'Arm',
-  },
-];
-
-const facetedFilters = [
-  {
-    columnId: 'os',
-    label: 'OS',
-    options: oses,
-  },
-  {
-    columnId: 'arch',
-    label: 'Arch',
-    options: arches,
-  },
-];
-
 async function getData(): Promise<BlenderInfo[]> {
   const data = await window.Blender.fetchVersion();
   return data;
@@ -61,7 +22,7 @@ export default function BlenderTablePage() {
 
   return (
     <div className="container mx-auto">
-      <DataTable columns={columns} data={data} facetedFilters={facetedFilters} />
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
