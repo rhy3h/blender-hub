34f8734b50671d6456d3f2cea6f0b95ae4ccb4aa
rhy3h <rhy3htw@gmail.com>
2025-06-29 22:27:37 +0800

    test: adjust test make it work

--- test/electron/ipcMain/BlenderIPCMain.test.ts
+++ test/electron/ipcMain/BlenderIPCMain.test.ts
@@ -1,49 +1,41 @@
 /* eslint-disable no-new */
 
 import {
+  afterAll,
+  beforeAll,
   describe,
   expect,
-  vi,
   it,
-  Mock,
-  beforeEach,
 } from 'vitest';
-
-import { ipcMain } from 'electron';
 
 import { FETCH_VERSION } from '@/electron/ipcInterface/mainProcess/BlenderIPCInterface';
 import { BlenderIPCMain } from '@/electron/ipcMain/BlenderIPCMain';
 
-vi.mock('electron', () => ({
-  ipcMain: {
-    handle: vi.fn(),
-  },
-}));
+import {
+  electronVitestMock,
+  getHandle,
+  resetIpcHandleMock,
+} from 'electron-mock/vitest';
+
+const mockWindow = electronVitestMock.BrowserWindow;
+const mockApp = electronVitestMock.app;
+const mockIpcMainInvokeEvent = electronVitestMock.IpcMainInvokeEvent;
 
 describe('BlenderIPCMain', () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
+  beforeAll(() => {
+    new BlenderIPCMain(mockWindow as unknown as Electron.BrowserWindow);
   });
 
-  it('should register FETCH_VERSION handler with correct response', async () => {
-    // Arrange
-    const handlerMock = vi.fn();
-    (ipcMain.handle as Mock).mockImplementation((_event, callback) => {
-      handlerMock.mockImplementation(callback as unknown as () => void);
-    });
+  afterAll(() => {
+    resetIpcHandleMock();
+  });
 
-    // Act
-    new BlenderIPCMain();
+  it('FETCH_VERSION', async () => {
+    const handler = getHandle(FETCH_VERSION);
 
-    // Assert
-    expect(ipcMain.handle).toHaveBeenCalledWith(
-      FETCH_VERSION,
-      expect.any(Function),
-    );
+    const result = await handler(mockIpcMainInvokeEvent);
 
-    const response = await handlerMock();
-
-    expect(response).toEqual([
+    expect(result).toEqual([
       {
         version: '4.6',
         status: 'Installed',
--- test/vitest.setup.ts
+++ test/vitest.setup.ts
@@ -1,3 +1,11 @@
+import { vi } from 'vitest';
+
 import { setupIpcMainHandleMock } from 'electron-mock/vitest';
 
 setupIpcMainHandleMock();
+
+vi.mock('electron-dl', () => ({
+  ipcMain: {
+    download: vi.fn(),
+  },
+}));
